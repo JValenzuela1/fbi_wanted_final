5341dfb4e4090198f1f08ea3fd56f636cc2a36fb
Luis Cortez <luiscortez@Luiss-MacBook-Air.local>
2023-05-15 10:27:24 -0700

    updated history file from Jacob

--- fbi-app/routes/history.js
+++ fbi-app/routes/history.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const db = require('../db');
 
 router.use((req, res, next) => {
-    console.log('Running Router Level Middleware For HISTORY');
-
-
-
     next();
 });
 
 router.get('/', async (req, res) => {
     try {
-        
+        const history = await db.find();
+
+        res.json(history);
+    } catch (error) {
+        res.status(500).json(error.toString());
+    } 
+});
+
+router.get('/:searchTerm', async (req, res) => {
+    try {
+        const { searchTerm } = req.params;
+
+        let history = await db.find(searchTerm);
+
+        if(!history) {
+            return res.status(404).json({ error: 'Search Term Does Not Exist in History'});
+        }
+        else {
+            await db.update(searchTerm, { lastSearched: new Date() });
+        }
+
+        res.json(history);
     } catch (error) {
         res.status(500).json(error.toString());
     } 
